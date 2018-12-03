a7cb54ea02cd6f976a51fd0e569ae353222e2156
Sebastian Erhardt <sebastian@ex-studios.net>
2018-12-03 11:06:08 +0100

    +/- fix tabs ident

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import time
 from flask import Flask
 from flask import make_response
 from pipeline import run
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-	# create response
-	response = make_response(run())
-	# filename
-	time_file_name = time.strftime("%Y-%m-%d-%H-%M-%S")
-	# set headers
-	response.headers.set('Content-Type', 'text/xml')
-	response.headers.set('Content-Disposition', 'attachment', filename='trend-seminar-xml-%s.xml' % time_file_name)
-	# return response
-	return response
+    # create response
+    response = make_response(run())
+    # filename
+    time_file_name = time.strftime("%Y-%m-%d-%H-%M-%S")
+    # set headers
+    response.headers.set('Content-Type', 'text/xml')
+    response.headers.set('Content-Disposition', 'attachment', filename='trend-seminar-xml-%s.xml' % time_file_name)
+    # return response
+    return response
